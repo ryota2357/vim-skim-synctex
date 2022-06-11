1195e27edb6771b54e8f9e536ab0b53fcfc5442d
2357 <61523777+ryota2357@users.noreply.github.com>
2022-06-11 23:51:17 +0900

    chore: remove unused deps

--- denops/synctex/lib/deps.ts
+++ denops/synctex/lib/deps.ts
@@ -1,6 +1,2 @@
 export type { Denops } from "https://deno.land/x/denops_std@v3.3.1/mod.ts";
-export * as func from "https://deno.land/x/denops_std@v3.3.1/function/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v3.3.1/helper/mod.ts";
-export * as buf from "https://deno.land/x/denops_std@v3.3.1/buffer/mod.ts";
-export { Lock } from "https://deno.land/x/async@v1.1.5/mod.ts";
 export * as unknown from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
