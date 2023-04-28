e35553442786907fe6ed1e37cee171b051944474
2357 <61523777+ryota2357@users.noreply.github.com>
2023-04-29 00:01:45 +0900

    chore: update deps

--- denops/synctex/lib/deps.ts
+++ denops/synctex/lib/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v4.0.0/helper/mod.ts";
-export * as func from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v4.0.0/autocmd/mod.ts";
-export * as variable from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.1.5/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v4.1.5/helper/mod.ts";
+export * as func from "https://deno.land/x/denops_std@v4.1.5/function/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v4.1.5/autocmd/mod.ts";
+export * as variable from "https://deno.land/x/denops_std@v4.1.5/variable/mod.ts";
 export * as unknown from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
