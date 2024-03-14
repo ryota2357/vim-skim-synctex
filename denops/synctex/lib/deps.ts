45807d5d43b2ed281daf9729967f6acad1415f02
GitHub <noreply@github.com>
2024-03-14 01:11:54 +0000

    Update deno dependencies

--- denops/synctex/lib/deps.ts
+++ denops/synctex/lib/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v6.3.0/helper/mod.ts";
-export * as func from "https://deno.land/x/denops_std@v6.3.0/function/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v6.3.0/autocmd/mod.ts";
-export * as variable from "https://deno.land/x/denops_std@v6.3.0/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.4.0/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
+export * as func from "https://deno.land/x/denops_std@v6.4.0/function/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.4.0/autocmd/mod.ts";
+export * as variable from "https://deno.land/x/denops_std@v6.4.0/variable/mod.ts";
 export { ensure, is } from "https://deno.land/x/unknownutil@v3.17.0/mod.ts";
