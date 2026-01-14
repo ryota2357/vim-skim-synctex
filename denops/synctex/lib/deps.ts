9ae7915b4bd15d31b401ba82e38a06ad7d4f2c83
ryota2357 <noreply@ryota2357.com>
2026-01-14 16:30:55 +0900

    chore: update deps

--- denops/synctex/lib/deps.ts
+++ denops/synctex/lib/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops, Entrypoint } from "jsr:@denops/std@^7.5.0";
-export * as helper from "jsr:@denops/std@^7.5.0/helper";
-export * as func from "jsr:@denops/std@^7.5.0/function";
-export * as autocmd from "jsr:@denops/std@^7.5.0/autocmd";
-export * as variable from "jsr:@denops/std@^7.5.0//variable";
+export type { Denops, Entrypoint } from "jsr:@denops/std@^8.2.0";
+export * as helper from "jsr:@denops/std@^8.2.0/helper";
+export * as func from "jsr:@denops/std@^8.2.0/function";
+export * as autocmd from "jsr:@denops/std@^8.2.0/autocmd";
+export * as variable from "jsr:@denops/std@^8.2.0//variable";
 export { ensure, is } from "jsr:@core/unknownutil@^4.3.0";
